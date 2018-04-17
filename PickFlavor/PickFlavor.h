231f5219e4c4d8cfcc98735862ac2671a06ef662
Nghia Hoang <nghia.hoang@hotmail.com>
2018-04-17 13:13:58 +0700

    Initial Commit

---
+++ PickFlavor/PickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  PickFlavor.h
+//  PickFlavor
+//
+//  Created by Nghia Hoang on 4/17/18.
+//  Copyright © 2018 Nghia Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PickFlavor.
+FOUNDATION_EXPORT double PickFlavorVersionNumber;
+
+//! Project version string for PickFlavor.
+FOUNDATION_EXPORT const unsigned char PickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickFlavor/PublicHeader.h>
+
+
